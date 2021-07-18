1061357b84b3fc8fcaff9e218fb38b313907bfd4
AprajitaChhawi <achhawip@gmail.com>
2021-07-18 15:09:01 +0530

    Create Day 10 counting sort.py

---
+++ Day 10 counting sort.py
@@ -0,0 +1,44 @@
+#User function Template for python3
+
+class Solution:
+    #Function to arrange all letters of a string in lexicographical 
+    #order using Counting Sort.
+    def countSort(self,arr):
+        count=[0]*26
+        for i in arr:
+            count[ord(i)-97]=count[ord(i)-97]+1
+        output=''
+        for i in range(0,26):
+            for j in range(0,count[i]):
+                output=output+chr(i+97)
+        return output
+            
+        # code here
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        arr = str(input())
+        ob = Solution()
+        print(ob.countSort(arr))
+
+# } Driver Code Ends
